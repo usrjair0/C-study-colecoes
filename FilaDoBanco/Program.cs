6db6287fb9b83fac9fbeb677b834849e38b03c5a
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-28 11:09:03 -0300

    add fila_pilha

--- FilaDoBanco/Program.cs
+++ FilaDoBanco/Program.cs
@@ -20,15 +20,31 @@ namespace FilaDoBanco
             Console.WriteLine($"{SAIR}-Sair do programa");
             Só posso sair do programa quando a fila acabar.
             */
+
+            /*
+             Array => problema é o limite
+             ArrayList => problema é o boxing e o unboxing
+             List<t> => manipular posição sem seguir o algoritimo FIFO (first in first out)
+             queue => Segue o algoritmo FIFO, mas cuidado com box e unboxing;
+             queue<t> => ideal para esse caso, pois segue o algoritmo fifo e por ser tipado
+             não faz o Boxing e unboxing.
+             stack => não pois segue o algoritmo LIFO - Last in first out, além disso faz o 
+             boxing e unboxing.
+             stack <t> => não pois segue o algoritmo LIFO
+             */
+
             Console.WriteLine("--Fila de Banco--");
+
+            const int ADICIONAR = 1, CHAMAR = 2, IMPRIMIR = 3, SAIRPROGRAMA = 4;
 
             Queue<string> fila = new Queue<string>();
             
 
-            Console.WriteLine($"1-Adicionar o nome do cliente");
-            Console.WriteLine($"2-Chamar o nome do cliente");
-            Console.WriteLine($"3-Imprimir a fila");
-            Console.WriteLine($"4-Sair do programa");
+            Console.WriteLine($"{ADICIONAR}-Adicionar o nome do cliente");
+            Console.WriteLine($"{CHAMAR}-Chamar o nome do cliente");
+            Console.WriteLine($"{IMPRIMIR}-Imprimir a fila");
+            Console.WriteLine($"{SAIRPROGRAMA}-Sair do programa");
+            
 
             bool sair = false;
             while(!sair){
@@ -38,12 +54,12 @@ namespace FilaDoBanco
 
                 switch (opcao)
                 {
-                    case 1:
+                    case ADICIONAR:
                         Console.Write("Digite o nome do cliente: ");
                         fila.Enqueue(Console.ReadLine());
                         break;
 
-                    case 2:
+                    case CHAMAR:
                         if (fila.Count != 0)
                         {
                             Console.WriteLine($"chamar {fila.Dequeue()}!");
@@ -52,15 +68,20 @@ namespace FilaDoBanco
                             Console.WriteLine("A fila está vazia!");
                         break;
 
-                    case 3:
-                        Console.WriteLine("A fila atual é composta por: ");
-                        foreach (string nome in fila)
-                        {
-                            Console.WriteLine(nome);
-                        };
+                    case IMPRIMIR:
+                        if(fila.Count > 0){
+                            Console.WriteLine("A fila atual é composta por: ");
+                            foreach (string nome in fila)
+                            {
+                                Console.WriteLine(nome);
+                            };
+                        }else {
+                            Console.WriteLine("A fila está vazia.");
+                        }
+                        
                         break;
 
-                    case 4:
+                    case SAIRPROGRAMA:
                         if (fila.Count == 0)
                         {
                             Console.WriteLine("Você irá sair do programa");
@@ -70,14 +91,19 @@ namespace FilaDoBanco
                             Console.WriteLine("Você não pode sair");
                         break;
 
+                    
                     default:
                         Console.WriteLine("Opção inválida");
                         break;
                 }
+
+                Console.WriteLine("Digite Enter para continuar");
+                Console.ReadLine();
+                Console.Clear();
             }
             
             Console.WriteLine("Digite Enter para sair");
-                Console.ReadLine();
+            Console.ReadLine();
 
         }
     }
---
+++ Fila_Pilha/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fila_Pilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa FIla Pilha");
+
+            Queue<int> fila = new Queue<int>();
+            Stack<int> pilha = new Stack<int>();
+            int continuar = 0;
+
+            do
+            {
+                Console.Write("Informe um numero Inteiro: ");
+                fila.Enqueue(int.Parse(Console.ReadLine()));
+                Console.Write("Gostaria de continuar? 1 - para sim e 2 - para nao: ");
+                continuar = int.Parse(Console.ReadLine());
+
+            }
+            while (continuar != 2);
+
+            while (fila.Count > 0)
+            {
+                pilha.Push(fila.Dequeue());
+            }
+
+            while (pilha.Count > 0)
+            {
+                Console.WriteLine(pilha.Pop());
+            }
+
+            /* foreach(int item in fila)
+                 pilha.Push(item);
+
+             foreach (int item in pilha)
+                 Console.WriteLine(item);
+            */
+            Console.WriteLine("Prima ENter para sair");
+            Console.ReadLine();
+        }
+    }
+}
