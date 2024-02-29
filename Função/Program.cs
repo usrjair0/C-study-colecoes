be2c9089065540d631d669eadb3fb9c416a171c2
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-29 12:06:18 -0300

    add chnages

---
+++ Função/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Função
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicio");
+            pularLinha(2);
+
+            Console.WriteLine("Meio");
+
+            pularLinha(5);
+
+            Console.WriteLine("Fim");
+            pularLinha(1);
+
+            Console.WriteLine("Digite Enter para sair");
+            Console.ReadLine();
+
+        }
+
+        //Essa função não necessita de retorno então uso void, poderia usar, mas não
+        //necessito
+        static void pularLinha(int quantidade)
+        {
+            for (int i = 0; i < quantidade; i++)
+            Console.WriteLine();
+        }
+        
+        static int somar(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+    }
+}
