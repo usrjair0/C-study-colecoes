6cd900a3520f2f866a3b63d43ee3c98332938623
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-29 10:05:05 -0300

    hashset

---
+++ MegaSena/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MegaSena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mega Sena");
+
+            Random random = new Random();
+
+            List<int> lista = new List<int>();
+            List<int> sorteados = new List<int>();
+
+            for (int i = 1; i < 61; i++)
+            {
+                lista.Add(i);
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                int sorteado = random.Next(1, 61); //primeiro entra, segundo nao
+
+                if (sorteados.Contains(sorteado))
+                {
+                    i--;
+                    continue;
+                }
+                else
+                {
+                    sorteados.Add(sorteado);
+                    lista.Remove(sorteado);
+                }
+
+            }
+            Console.Write("Numeros Sorteados: ");
+            foreach (var item in sorteados)
+            {
+
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Numeros não Sorteados: ");
+            foreach (var item in lista)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("\nPressione Enter para sair");
+            Console.ReadLine();
+
+           
+        }
+    }
+}
---
+++ MegaSena_Hashset/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MegaSena_Hashset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mega Sena");
+
+            Random random = new Random();
+
+            List<int> naoSorteados = new List<int>(); 
+
+            HashSet<int> sorteados = new HashSet<int>();//HasheSet não adiciona numeros repetidos
+
+            for (int i = 1; i < 61; i++)
+            {
+                naoSorteados.Add(i);
+            }
+            
+            for (int i = 0; i < 6; i++)
+            {
+                int sorteado = random.Next(1, 61); //primeiro entra, segundo nao
+
+                if (sorteados.Add(sorteado))
+                {
+                    i--;
+                    continue;
+                }
+                sorteados.Add(sorteado);
+            }
+
+            Console.Write("Numeros Sorteados: ");
+
+            foreach (var item in sorteados)
+            {
+
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Numeros não Sorteados: ");
+            foreach (var item in naoSorteados)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("\nPressione Enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
